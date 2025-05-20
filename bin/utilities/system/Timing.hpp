06ede5efef7cc10a93f8197c7d032e9ec6e89a75
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-05-20 10:52:37 -0500

    Create Timing.hpp

---
+++ bin/utilities/system/Timing.hpp
@@ -0,0 +1,56 @@
+/**
+ * File: Timing.hpp
+ * Author: Antonius Torode
+ * Creation Date: 03/09/2021
+ * Description: Declares system-level timing utilities intended to provide a unified 
+ *    interface for time-based operations such as delays, timing measurements, and 
+ *    duration conversions. This header is part of the system utilities module and 
+ *    may be extended with additional timing-related functionality.
+ */
+
+namespace system
+{
+    /**
+     * Provides static timing-related utility functions for thread delays 
+     * and other time-based operations. Designed as a centralized interface 
+     * for timing utilities within the system module, allowing easy expansion 
+     * with additional timing features.
+     */
+    class Timing
+    {
+    public:
+        /**
+         * Main default constructor for the SystemUtils object.
+         */
+        SystemUtils() = default;
+    
+        /**
+         * Main destructor for the SystemUtils object.
+         */
+        ~SystemUtils() = default;
+    
+        /**
+         * Causes the current thread to sleep for some number of milliseconds.
+         * @param time[int] - Time to sleep in milliseconds.
+         */
+        static void sleepMilliseconds(int time);
+    
+        /**
+         * Causes the current thread to sleep for some number of minutes.
+         * @param time[int] - Time to sleep in minutes.
+         */
+        static void sleepMinutes(int time);
+    
+        /**
+         * Causes the current thread to sleep for some number of seconds.
+         * @param time[int] - Time to sleep in seconds.
+         */
+        static void sleepSeconds(int time);
+  
+        /**
+         * Causes the current thread to sleep for some number of fractional seconds.
+         * @param time[double] - Time to sleep in fractional seconds.
+         */
+        static void sleepSeconds(double time);
+    };
+} // namespace system
