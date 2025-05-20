ac02e42a78017ed25d01c071e2d2653d62d14d6f
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-05-20 13:55:09 -0500

    Create MathUtils.hpp

---
+++ bin/utilities/math/MathUtils.hpp
@@ -0,0 +1,38 @@
+/**
+ * File: MathUtils.hpp
+ * Author: Antonius Torode
+ * Created on: 03/01/2021
+ * Description: This file provides a collection of general-purpose mathematical utility functions 
+ *     for various common operations such as random number generation, dice rolling, and other 
+ *     math-related tasks. It serves as a centralized place for reusable math helpers 
+ *     across different projects or modules.
+ */
+#pragma once
+
+namespace math
+{
+    /**
+     * Generates a random integer between two values.
+     * @param min[int] - lower bound value to use.
+     * @param max[int] - higher bound to use.
+     * @param seed[int] - a seed to randomize output.
+     * @param useTime[bool] - uses teh time to determine the seed (default = true).
+     * @return
+     */
+    static int randomInt(int min, int max, int seed, bool useTime = true);
+
+    /**
+     * Rolls a dice based on input.
+     * @param input[std::string&] - Input string. Must be of the form #d#, #d##, ##d##, etc.
+     * @return [int] - returns the total dice roll.
+     */
+    static int roll(const std::string &input);
+
+    /**
+     * Returns a 1dXX dice roll total output.
+     * @param xx[int] - Size of dice.
+     * @param seed[int] - A seed to use for randomization.
+     * @return [int] returns the total.
+     */
+    static int rolldXX(int xx, int seed);
+} // namespace math
