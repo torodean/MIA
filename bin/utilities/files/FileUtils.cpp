cb21bd83eb138368ade85993a3cc7c19bc0a1774
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-05-21 15:11:48 -0500

    Update FileUtils.cpp

--- bin/utilities/files/FileUtils.cpp
+++ bin/utilities/files/FileUtils.cpp
@@ -22,66 +22,70 @@ using std::string;
 using std::cout;
 using std::endl;
 
-string getRandomLineOfFile(string& fileName)
+namespace files
 {
-    int counter = 0;
-    string output;
-    std::ifstream file(fileName);
 
-    while (getline(file, output))
+    string getRandomLineOfFile(string& fileName)
     {
-        counter++;
-    }
-    file.close();
-
-    std::ifstream file2(fileName);
-    // Create a seeded random engine (usually done once, not every call)
-    static std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());    
-    // Generate a random integer between 1 and counter (inclusive)
-    int random = std::uniform_int_distribution<int>(1, counter)(rng);
-    counter = 0;
-
-    while (getline(file2, output))
-    {
-        counter++;
-        if(counter == random)
+        int counter = 0;
+        string output;
+        std::ifstream file(fileName);
+    
+        while (getline(file, output))
         {
-            return output;
+            counter++;
+        }
+        file.close();
+    
+        std::ifstream file2(fileName);
+        // Create a seeded random engine (usually done once, not every call)
+        static std::mt19937 rng(std::chrono::steady_clock::now().time_since_epoch().count());    
+        // Generate a random integer between 1 and counter (inclusive)
+        int random = std::uniform_int_distribution<int>(1, counter)(rng);
+        counter = 0;
+    
+        while (getline(file2, output))
+        {
+            counter++;
+            if(counter == random)
+            {
+                return output;
+            }
+        }
+        return "ERROR"; // @TODO - Add MIA Exception here.
+    }
+    
+    void printRandomLinesFromFile(std::string filePath, int numberOfLines)
+    {
+        // Grabs the file.
+        std::ifstream file(filePath, std::ifstream::in);
+    
+        // Checks if the file exists and runs the code.
+        if (file.good())
+        {
+            string line;
+            std::vector<string> lines;
+    
+            while(getline(file,line))
+            {
+                //cout << line << endl;
+                lines.push_back(line);
+            }
+    
+            int listSize = lines.size();
+            srand(time(nullptr));
+            int randomNum;
+    
+            for(int i=0; i<numberOfLines; i++)
+            {
+                randomNum = rand() % listSize;
+                cout << lines[randomNum] << endl;
+            }
+        } 
+        else 
+        {
+            // @TODO - change this to use the new MIA Exception class.
+            error::returnError(41404);
         }
     }
-    return "ERROR"; // @TODO - Add MIA Exception here.
-}
-
-void printRandomLinesFromFile(std::string filePath, int numberOfLines)
-{
-    // Grabs the file.
-    std::ifstream file(filePath, std::ifstream::in);
-
-    // Checks if the file exists and runs the code.
-    if (file.good())
-    {
-        string line;
-        std::vector<string> lines;
-
-        while(getline(file,line))
-        {
-            //cout << line << endl;
-            lines.push_back(line);
-        }
-
-        int listSize = lines.size();
-        srand(time(nullptr));
-        int randomNum;
-
-        for(int i=0; i<numberOfLines; i++)
-        {
-            randomNum = rand() % listSize;
-            cout << lines[randomNum] << endl;
-        }
-    } 
-    else 
-    {
-        // @TODO - change this to use the new MIA Exception class.
-        error::returnError(41404);
-    }
-}
+} // namespace files
