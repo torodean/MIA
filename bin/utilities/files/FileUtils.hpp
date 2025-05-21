8dfe050649f962ef9c9acfb60cf6ab3d55eca8ae
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-05-21 14:44:08 -0500

    Update FileUtils.hpp

--- bin/utilities/files/FileUtils.hpp
+++ bin/utilities/files/FileUtils.hpp
@@ -4,21 +4,25 @@
  * Created on: 03/07/2021
  * Description: Provides utility functions for reading, writing, and processing files.
  */
+#pragma once
+
+#include <string>
 
 namespace files
 {
     /**
      * Returns the random line of a specified text file.
-     * @param fileName[std::string] Input file to parse.
-     * @return [std::string] Returns a random line of the file.
+     * @param fileName[std::string] - Input file to parse.
+     * @return [std::string] - Returns a random line of the file.
      */
     static std::string getRandomLineOfFile(std::string& fileName);
 
     /**
      * Takes a file as an input and prints an input number of random lines from it.
-     * @param useDefaultPath[bool] - true if using the default MIA inputFilePath from the MIAConfig.MIA file.
-     * @param numberOfLines[int] - The number of lines to return
+     * @param filePath[std::string] - the input file path to use.
+     * @param numberOfLines[int] - The number of lines to return.
      */
-    static void printRandomLinesFromFile(bool useDefaultPath, int numberOfLines);
+    [[deprecated("This method has changed. Review the new usage.")]]
+    static void printRandomLinesFromFile(std::string filePath, int numberOfLines);
 
 } // namespace files
