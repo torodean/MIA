24f2abfbe74f885839504738bb79494fb9c43e1d
Antonius Torode <awtorode@gmail.com>
2025-05-23 16:32:58 -0500

    Added some docs

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,3 +2,7 @@
 
 # This script is used for isntalling dependencies and performing
 # various setup which is needed for the build and install.
+
+
+sudo apt-get install libxdo-dev
+sudo apt-get install libmysqlcppconn-dev
