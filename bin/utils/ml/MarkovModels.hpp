2482e9eb0f05895e881c4852b60f524d7a1e3a22
Antonius Torode <24546058+torodean@users.noreply.github.com>
2025-06-03 14:28:17 -0500

    Update MarkovModels.hpp

--- bin/utils/ml/MarkovModels.hpp
+++ bin/utils/ml/MarkovModels.hpp
@@ -78,4 +78,108 @@ namespace markov_models
 
         return prob_matrix;
     }
+
+    /**
+     * Retrieves the successor probabilities for a given input state.
+     * 
+     * @tparam T - The type of the state.
+     * @param prob_matrix [const ProbabilityMatrix<T>&] - The probability matrix generated 
+     *     by generate_probability_matrix.
+     * @param state [const T&] - The current state for which successors are requested.
+     * @return A const reference to the unordered_map of successors and their probabilities.
+     *         Returns an empty static map if the state is not found.
+     */
+    template <typename T>
+    const std::unordered_map<T, double>& get_transitions(
+        const ProbabilityMatrix<T>& prob_matrix, 
+        const T& state) 
+    {
+        static const std::unordered_map<T, double> empty_map{};
+        auto it = prob_matrix.find(state);
+        if (it != prob_matrix.end()) 
+        {
+            return it->second;
+        }
+        return empty_map;
+    }
+
+    /**
+     * Checks if a given state exists in the probability matrix.
+     * @tparam T The type of the state.
+     * @param matrix[const ProbabilityMatrix<T>&] - The probability matrix.
+     * @param state[const T&] - The state to check.
+     * @return [bool] - true if the state exists in the matrix, false otherwise.
+     */
+    template <typename T>
+    bool has_state(const ProbabilityMatrix<T>& matrix, const T& state) 
+    {
+        return matrix.find(state) != matrix.end();
+    }
+
+    /**
+     * Returns a vector containing all states present in the probability matrix.
+     * 
+     * @tparam T The type of the state.
+     * @param matrix[const ProbabilityMatrix<T>&] - The probability matrix.
+     * @return [std::vector<T>] - A vector of all states.
+     */
+    template <typename T>
+    std::vector<T> get_states(const ProbabilityMatrix<T>& matrix) 
+    {
+        std::vector<T> states;
+        states.reserve(matrix.size());
+        for (const auto& [state, _] : matrix) 
+        {
+            states.push_back(state);
+        }
+        return states;
+    }
+
+    /**
+     * Samples the next state from the probability distribution of a given state.
+     * Uses a random number generator to select a successor state weighted by
+     * the transition probabilities.
+     * 
+     * @tparam T The type of the state.
+     * @param matrix[const ProbabilityMatrix<T>&] - The probability matrix.
+     * @param state[const T&] - The current state from which to sample the next state.
+     * @return The sampled next state.
+     */
+    template <typename T>
+    T sample_next_state(const ProbabilityMatrix<T>& matrix, const T& state) 
+    {
+        // TODO - implement this.
+    }
+
+    /**
+     * Prints the probability matrix to the specified output stream.
+     * Useful for debugging or visualization.
+     * @tparam T The type of the state.
+     * @param matrix[const ProbabilityMatrix<T>&] - The probability matrix to print.
+     * @param os[std::ostream&] - The output stream to write to (defaults to std::cout).
+     */
+    template <typename T>
+    void print_probability_matrix(const ProbabilityMatrix<T>& matrix, std::ostream& os = std::cout) 
+    {
+        for (const auto& [state, transitions] : matrix) 
+        {
+            os << state << " -> { ";
+            for (const auto& [next, prob] : transitions) 
+            {
+                os << next << ": " << prob << ", ";
+            }
+            os << "}\n";
+        }
+    }
+
+    /**
+     * Clears all entries from the probability matrix.
+     * @tparam T The type of the state.
+     * @param matrix[ProbabilityMatrix<T>&] - The probability matrix to clear.
+     */
+    template <typename T>
+    void clear_matrix(ProbabilityMatrix<T>& matrix) 
+    {
+        matrix.clear();
+    }
 } // namespace markov_models
