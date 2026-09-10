382d785b06e75e4cef132d580b282962f5d45198
Antonius Torode <24546058+torodean@users.noreply.github.com>
2026-09-10 09:52:46 -0500

    Define install_app function in apps.cmake
    I think this parameter accidentally got deleted just before committing... because it was working last time I ran it locally and the parameter was not empty.

--- cmake/apps.cmake
+++ cmake/apps.cmake
@@ -1,6 +1,6 @@
 # Installs the specified target files into the system directory when performing 
 # a system install, or into the release directory when building a release.
-function( )
+function( install_app )
     if(SYSTEM_INSTALL)    
         install(TARGETS ${ARGV} DESTINATION ${APP_INSTALL_LOCATION})
     elseif(RELEASE_BUILD)
