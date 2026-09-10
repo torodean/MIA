dbef633c4c3a53c15a702025c7230b5100f13fc5
Antonius Torode <awtorode@gmail.com>
2026-09-10 00:14:26 -0500

    Added docs about new python features

--- cmake/apps.cmake
+++ cmake/apps.cmake
@@ -1,6 +1,6 @@
 # Installs the specified target files into the system directory when performing 
 # a system install, or into the release directory when building a release.
-function(install_app)
+function( )
     if(SYSTEM_INSTALL)    
         install(TARGETS ${ARGV} DESTINATION ${APP_INSTALL_LOCATION})
     elseif(RELEASE_BUILD)
