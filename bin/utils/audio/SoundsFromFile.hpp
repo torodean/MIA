1c3f2a48eb913eda0eca08bc594f867c5c981798
Antonius Torode <24546058+torodean@users.noreply.github.com>
2026-09-18 10:24:48 -0500

    Update documentation for audio file methods

--- bin/utils/audio/SoundsFromFile.hpp
+++ bin/utils/audio/SoundsFromFile.hpp
@@ -15,57 +15,65 @@
 namespace audio
 {
 	/**
-	 * This method returns if the audio file is a supported type or not.
+	 * @brief Returns if the audio file is a supported type or not.
+	 *
 	 * This delegates to the isASupportedType(const FileMetaData&) overload.
+	 *
 	 * @param fileName The file name to check for support. This uses the
 	 *        file extension to perform the check.
-     * @param verboseMode Enables verboseMode output (default = false).
+	 * @param verboseMode Enables verboseMode output (default = false).
 	 * @return true if the type is supported.
 	 */
 	bool isASupportedType(const std::string& fileName,
 	                      bool verboseMode = false);
-	
+
 	/**
-	 * This method returns if the audio file is a supported type or not.
+	 * @brief Returns if the audio file is a supported type or not.
+	 *
 	 * When a new type is supported, it must be added to this method.
+	 *
 	 * @param data The metaData of the file to check. This can be
 	 *        constructed with the getFileMetaData() method.
-     * @param verboseMode Enables verboseMode output (default = false).
+	 * @param verboseMode Enables verboseMode output (default = false).
 	 * @return true if the type is supported.
 	 */
 	bool isASupportedType(const files::FileMetaData& data,
 	                      bool verboseMode = false);
-	
+
 	/**
-	 * Attempts to play a sound from a file.
+	 * @brief Attempts to play a sound from a file.
+	 *
 	 * @note This is a blocking call and will block the thread until
 	 *       the audio file is finished playing.
-	 * @param fileName The file to try playing a sound from. This
-	 *        must be a valid audio file of a supported type.
+	 *
+	 * @param fileName The file to try playing a sound from. This must
+	 *        be a valid audio file of a supported type.
 	 * @return true on success, false otherwise.
 	 * @throws MIAException when ran on an unsupported OS.
 	 */
 	bool playSoundFromFile(const std::string& fileName);
-	
+
+	// @TODO - Implement on Linux.
 	/**
-	 * Attempts to play a sound from a file.
+	 * @brief Attempts to play a sound from a file.
+	 *
 	 * @note This is a non-blocking call. Playback continues in the background
 	 *       until the audio file finishes or stopSound() is called.
+	 *
 	 * @param fileName The file to try playing a sound from. This must be a
 	 *        valid audio file of a supported type.
 	 * @return true on success, false otherwise.
 	 * @throws MIAException when ran on an unsupported OS.
-	 * TODO - IN DEVELOPMENT!
 	 */
 	bool playSoundFromFileAsync(const std::string& fileName);
 
+	// @TODO - Implement on Linux.
 	/**
-	 * Stops the currently playing sound.
+	 * @brief Stops the currently playing sound.
 	 * @param fadeOutMs The duration of the fade-out in milliseconds. A value
 	 *        of 0 stops playback immediately.
 	 * @return true if successful or if no sound was playing, false otherwise.
 	 * @throws MIAException when ran on an unsupported OS.
-	 * TODO - IN DEVELOPMENT!
 	 */
 	bool stopSound(uint32_t fadeOutMs = 0, bool verboseMode = false);
 } // namespace audio
